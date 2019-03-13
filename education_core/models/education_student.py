f2b3fea95d17b7fb8363efad997b232452aa98ac
Mauro Estrella <andesasoft@gmail.com>
2019-03-13 11:52:55 -0500

    Cambios en traduccion es_EC en e_core y e_exam, modificacion en .gitignore para que no siga a ext de eclipse, correccion de campo mother_tongue eliminando domain no necesario

--- education_core/models/education_student.py
+++ education_core/models/education_student.py
@@ -93,7 +93,7 @@ class EducationStudent(models.Model):
     per_country_id = fields.Many2one('res.country', string='Country', ondelete='restrict')
     medium = fields.Many2one('education.medium', string="Medium", required=True)
     sec_lang = fields.Many2one('education.subject', string="Second language", required=True, domain=[('is_language', '=', True)])
-    mother_tongue = fields.Many2one('education.mother.tongue', string="Mother Tongue", required=True, domain=[('is_language', '=', True)])
+    mother_tongue = fields.Many2one('education.mother.tongue', string="Mother Tongue", required=True,)
     caste_id = fields.Many2one('religion.caste', string="Caste")
     religion_id = fields.Many2one('religion.religion', string="Religion")
     is_same_address = fields.Boolean(string="Is same Address?")
